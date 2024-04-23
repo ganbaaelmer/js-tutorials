46c5991ccd30834b25f5a8c1ce7c10185030b248
Ganbaa <elmerganbaa@gmail.com>
2024-04-23 14:33:44 +0800

    shooshideh function nemsen

--- lesson38 github/dice-game/app.js
+++ lesson38 github/dice-game/app.js
@@ -7,18 +7,29 @@ var scores = [0, 0];
 //toglogchiin eeljindee tsugluulj baigaag hadgalah huvisagch
 var roundScore = 0;
 
-//shoo ali talaar buusniig hadgalah huvisagch. 1-6 shoo
-// var dice = Math.floor(Math.random() * 6) + 1;
-// window.document.querySelector("#score-0").textContent = dice;
-
-// window.document.querySelector("#score-1").innerHTML = "<em>YES!</em>";
-
-// console.log("Shoo engej buulaa :>> ", dice);
-// shoonii zuragiig alga bolgoloo:
-document.querySelector(".dice").style.display = "none";
-
 // program ehlehed beltgii:
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+// document.querySelector("#score-0").textContent = 0;
+// document.querySelector("#score-1").textContent = 0;
+// document.querySelector("#current-0").textContent = 0;
+// document.querySelector("#current-1").textContent = 0;
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+//document.querySelector(".dice") -g bainga duudahgui neg variabled joorloj awaad ashiglaad yawchii.Engevel programm dahin dahin querry hhgui mash hurdan bolno.
+var diceDom = document.querySelector(".dice");
+
+// shoonii zuragiig alga bolgoloo:
+diceDom.style.display = "none";
+
+// shooShid -g arilgaad anonymous function nemchlee
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  //shoo ali talaar buusniig hadgalah huvisagch. 1-6 shoo
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // shoonii zurag haruulah:
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+  //   alert("Shoo buulaa : " + diceNumber);
+});
